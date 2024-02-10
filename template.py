29f2d0adbc04a166ee0a90f5e137b1511d7bc119
Samrao2607 <sameery2607@gmail.com>
2024-02-10 12:40:26 +0530

    template file updated

--- template.py
+++ template.py
@@ -26,7 +26,7 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating Directory; {filedir} for the file  {filename}")
     
-    if (not os.path.exists(filepath)) or (os.path.getsize(filename)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filename}")
